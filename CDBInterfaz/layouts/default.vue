0460aea8fb2c39734970f86753cdc5284f6f90a8
Fernando Sagastume <Fernando Sagastume@DESKTOP-6B11A69.local>
2023-11-28 21:52:39 -0600

    Proyecto concluido.

--- CDBInterfaz/layouts/default.vue
+++ CDBInterfaz/layouts/default.vue
@@ -1,39 +1,13 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-      dark
-      color="primary"
-      :width="($vuetify.breakpoint.name === 'sm' ||
-      $vuetify.breakpoint.name === 'xs') ? 250 : 350"
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar dark color="primary" elevation="0"
                :clipped-left="clipped" fixed app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title class="hidden-sm-and-down">
-        <h4>{{$store.state.nombreRuta}}</h4>
+        <h3>{{$store.state.nombreRuta}}</h3>
+      </v-toolbar-title>
+      <v-toolbar-title class="hidden-md-and-up">
+        <h5>{{$store.state.nombreRuta}}</h5>
       </v-toolbar-title>
     </v-app-bar>
     <v-main>
@@ -67,11 +41,6 @@ export default {
           icon: 'fa fa-code-branch',
           title: 'Clasificador de Basura',
           to: '/'
-        },
-        {
-          icon: 'fa fa-recycle',
-          title: 'Acerca del Reciclaje',
-          to: '/reciclaje'
         }
       ],
       miniVariant: false,
--- CDBInterfaz/pages/index.vue
+++ CDBInterfaz/pages/index.vue
@@ -75,18 +75,51 @@
             <v-card style="border-radius:15px;" elevation="0" outlined class="pa-6"
                     v-if="imagePreview"
             >
-              <v-img :src="imagePreview" />
               <v-toolbar elevation="0" dense color="transparent" class="hidden-sm-and-down" v-if="prediccion">
                 <v-spacer/>
                 <h2> Este objeto debe reciclarse como: {{ this.prediccion }} </h2>
                 <v-spacer/>
               </v-toolbar>
-
               <v-toolbar elevation="0" dense color="transparent" class="hidden-md-and-up" v-if="prediccion">
                 <v-spacer/>
                 <h4> Este objeto debe reciclarse como: {{ this.prediccion }} </h4>
                 <v-spacer/>
               </v-toolbar>
+              <v-card-text class="text-center">
+                <v-img :src="imagePreview" :max-height="300" :max-width="300" class="mx-auto my-auto"/>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+          <v-col cols="12" lg="10" md="8" sm="12">
+            <v-card style="border-radius:15px;" elevation="0" outlined class="pa-6"
+                    v-if="prediccion"
+            >
+              <v-toolbar elevation="0" dense color="transparent" class="hidden-sm-and-down">
+                <v-spacer/>
+                <h2> Instrucciones para el reciclaje </h2>
+                <v-spacer/>
+              </v-toolbar>
+              <v-toolbar elevation="0" dense color="transparent" class="hidden-md-and-up">
+                <v-spacer/>
+                <h4> Instrucciones para el reciclaje </h4>
+                <v-spacer/>
+              </v-toolbar>
+              <v-card-text class="text-center">
+                <div>
+                  <v-list>
+                    <v-list-item-group v-if="instrucciones.length > 0">
+                      <v-list-item v-for="(instruccion, index) in instrucciones" :key="index">
+                        <v-list-item-icon> <v-icon color="black">fa fa-arrow-right</v-icon> </v-list-item-icon>
+                        <v-list-item-content>
+                          <v-list-item-title class="text-wrap">{{ instruccion.title }}</v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list>
+                </div>
+              </v-card-text>
             </v-card>
           </v-col>
         </v-row>
@@ -108,6 +141,7 @@
         imagePreview: null,
         prediccion: null,
         error: null,
+        instrucciones: [],
         rules: {
           imagen:{
             required: value => !!value || 'La imagen es obligatoria'
@@ -138,14 +172,41 @@
 
           await this.$api.postFiles("", params, true).then(data => {
 
-            if(data.prediccion === 'carton'){
-              this.prediccion = 'cartón'
-            }
-            else if(data.prediccion === 'plastico'){
-              this.prediccion = 'plástico'
-            }
-            else{
-              this.prediccion = data.prediccion
+            if (data.prediccion === 'carton') {
+              this.prediccion = 'cartón';
+              this.instrucciones = [];
+              this.instrucciones.push({ title: "Asegúrate de que el cartón esté limpio y sin residuos de comida.", value: 1 });
+              this.instrucciones.push({ title: "Separa cualquier cinta adhesiva o etiquetas antes de reciclar.", value: 2 });
+              this.instrucciones.push({ title: "Puedes reciclar cajas de cartón, envases y otros productos de cartón.", value: 3 });
+            } else if (data.prediccion === 'plastico') {
+              this.prediccion = 'plástico';
+              this.instrucciones = [];
+              this.instrucciones.push({ title: "Verifica los códigos de reciclaje en los envases de plástico (números del 1 al 7).", value: 1 });
+              this.instrucciones.push({ title: "Enjuaga los envases de plástico y retira las tapas antes de reciclar.", value: 2 });
+              this.instrucciones.push({ title: "Algunos plásticos, como las botellas de agua, son generalmente reciclables.", value: 3 });
+            } else {
+              this.prediccion = data.prediccion;
+              if (data.prediccion === 'vidrio') {
+                this.instrucciones = [];
+                this.instrucciones.push({ title: "Limpia los envases de vidrio para eliminar cualquier residuo.", value: 1 });
+                this.instrucciones.push({ title: "Separa el vidrio por colores: transparente, verde y ámbar.", value: 2 });
+                this.instrucciones.push({ title: "Recicla botellas, frascos y otros envases de vidrio.", value: 3 });
+              } else if (data.prediccion === 'metal') {
+                this.instrucciones = [];
+                this.instrucciones.push({ title: "Recicla latas de aluminio y acero.", value: 1 });
+                this.instrucciones.push({ title: "Asegúrate de enjuagar los envases de metal para eliminar cualquier residuo.", value: 2 });
+                this.instrucciones.push({ title: "Puedes reciclar latas de refrescos, latas de alimentos y otros productos de metal.", value: 3 });
+              } else if (data.prediccion === 'papel') {
+                this.instrucciones = [];
+                this.instrucciones.push({ title: "Recicla papel limpio y seco.", value: 1 });
+                this.instrucciones.push({ title: "Separa el papel por tipo: periódicos, revistas, cartón, etc.", value: 2 });
+                this.instrucciones.push({ title: "Evita reciclar papel contaminado con alimentos o productos químicos.", value: 3 });
+              } else if (data.prediccion === 'basura') {
+                this.instrucciones = [];
+                this.instrucciones.push({ title: "Intenta reducir la cantidad de residuos que generas.", value: 1 });
+                this.instrucciones.push({ title: "Separa los residuos no reciclables de los reciclables.", value: 2 });
+                this.instrucciones.push({ title: "Coloca los desechos no reciclables en bolsas adecuadas y asegúrate de que sean manejados correctamente.", value: 3 });
+              }
             }
 
           }).catch(data => {
